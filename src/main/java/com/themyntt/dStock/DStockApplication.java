a5856bccc71a5525d88119cdc3908e87e51d43cb
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-16 22:18:14 -0300

    feat: initial commit

---
+++ src/main/java/com/themyntt/dStock/DStockApplication.java
@@ -0,0 +1,13 @@
+package com.themyntt.dStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/themyntt/dStock/DStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themyntt.dStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
