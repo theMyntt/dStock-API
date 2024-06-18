d5686d84e4b37014333864bdde8a9b752d8952f7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-17 21:21:27 -0300

    docs: UserLoginDTO schema

--- src/main/java/com/themyntt/dStock/dtos/UserLoginDTO.java
+++ src/main/java/com/themyntt/dStock/dtos/UserLoginDTO.java
@@ -1,6 +1,20 @@
 package com.themyntt.dStock.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Entrada de dados do usuário para login")
 public class UserLoginDTO {
-    public int id;
-    public String senha;
+    @Schema(description = "Código de acesso", example = "1")
+    private int id;
+
+    @Schema(description = "Senha do usuário", example = "12345678")
+    private String senha;
 }
