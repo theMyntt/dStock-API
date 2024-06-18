010e80b0a3bb293e9601e5c7e198f6147577aa15
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-17 21:20:30 -0300

    docs: swagger basic documentation

--- src/main/java/com/themyntt/dStock/DStockApplication.java
+++ src/main/java/com/themyntt/dStock/DStockApplication.java
@@ -1,8 +1,26 @@
 package com.themyntt.dStock;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "dStock API",
+				version = "0.1",
+				description = "API para gerenciamento de estoque",
+				contact = @Contact(
+						name = "Gabriel Araújo Lima",
+						email = "gabriel.araujo2902@outlook.com"
+				)
+		),
+		servers = {
+				@Server(url = "http://localhost:8080", description = "Servidor local")
+		}
+)
 @SpringBootApplication
 public class DStockApplication {
 
